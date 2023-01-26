e76e829143e9a2e8d8528cf3cb5117125d8c191b
Ramineni Nandu Varma <123319320+Nandu-ramineni@users.noreply.github.com>
2023-01-26 15:26:29 +0530

    Add files via upload

---
+++ payment.js
@@ -0,0 +1,14 @@
+/*This is basic email js you can import your code here to do the payment process with back end*/
+function sendEmail() {
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
